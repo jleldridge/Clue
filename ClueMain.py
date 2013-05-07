6133cb116e21c9f0f77266130f274b3574067185
jleldridge <jleldridge27@gmail.com>
2013-05-07 11:45:48 -0400

    Made the candidate lists global variables.

--- ClueMain.py
+++ ClueMain.py
@@ -1,13 +1,12 @@
 import sys, random
 
-def main():
-    #set up possible weapons, rooms, and people
-    people = ['Mustard', 'Plum', 'Scarlett', 'Green', 'Peacock', 'White']
-    weapons = ['Dagger', 'Rope', 'Lead Pipe', 'Candlestick', 'Revolver', 
+#set up possible weapons, rooms, and people
+people = ['Mustard', 'Plum', 'Scarlett', 'Green', 'Peacock', 'White']
+weapons = ['Dagger', 'Rope', 'Lead Pipe', 'Candlestick', 'Revolver', 
         'Wrench']
-    rooms = ['Study', 'Hall', 'Lounge', 'Library', 'Billiard', 'Conservatory',
+rooms = ['Study', 'Hall', 'Lounge', 'Library', 'Billiard', 'Conservatory',
         'Ballroom', 'Kitchen', 'Dining']
-    adjacentRooms = {'Study': ['Study', 'Hall', 'Library'], 
+adjacentRooms = {'Study': ['Study', 'Hall', 'Library'], 
         'Hall': ['Hall', 'Study', 'Lounge'],
         'Lounge': ['Lounge', 'Hall', 'Dining'], 
         'Dining': ['Dining', 'Lounge', 'Kitchen'],
@@ -16,12 +15,12 @@ def main():
         'Conservatory': ['Conservatory', 'Ballroom', 'Billiard'],
         'Billiard': ['Billiard', 'Conservatory', 'Library'], 
         'Library': ['Library', 'Billiard', 'Study']}
-    #Set up the possible murder weapons for each person based on the logic rules.
-    #To account for double probability of white using blunt weapons, added
-    #an extra of each blunt weapon to her possible weapons.
-    #Green will only use the pipe or wrench anyway, so no need to account for
-    #his higher probability of using blunt weapons.
-    weaponsPerMurderer = {
+#Set up the possible murder weapons for each person based on the logic rules.
+#To account for double probability of white using blunt weapons, added
+#an extra of each blunt weapon to her possible weapons.
+#Green will only use the pipe or wrench anyway, so no need to account for
+#his higher probability of using blunt weapons.
+weaponsPerMurderer = {
         'Mustard': ['Dagger', 'Rope', 'Lead Pipe', 'Candlestick', 'Wrench'],
         'Plum': ['Rope', 'Lead Pipe', 'Candlestick', 'Revolver', 'Wrench'],
         'Scarlett': ['Rope', 'Lead Pipe', 'Candlestick', 'Revolver', 'Wrench'],
@@ -31,12 +30,15 @@ def main():
         'White': ['Rope', 'Dagger', 'Lead Pipe', 'Lead Pipe','Candlestick', 
             'Candlestick', 'Revolver', 'Wrench', 'Wrench']}
 
+def detective():
+    players = {'p1':[], 'p2':[], 'p3':[], 'p4':[], 'p5':[], 'p6'[]}
+
+def main():    
     #Pick a weapon, room, and murderer based on the logic rules
     murderer = people[random.randint(0, len(people)-1)]
     startRoom = rooms[random.randint(0, len(rooms)-1)]
     endRoom = None
     if murderer == 'Scarlett' or murderer == 'Peacock' or murderer == 'White':
-        print("going here\n")
         endRoom = adjacentRooms[startRoom][random.randint(0, 2)]
     else:
         endRoom = rooms[random.randint(0, len(rooms)-1)]
@@ -45,8 +47,6 @@ def main():
     
     print("murderer: ", murderer, "\nweapon: ", murderWeapon, 
         "\nstart room: ", startRoom, "\nend room: ", endRoom, "\n")
-
-
     
 if __name__ == '__main__':
     try:
